d225255e6f5d7d0cee9570492a35294f0ba27062
Marie Burns <mcburns2222@gmail.com>
2018-11-20 01:16:26 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,10 +16,10 @@ def line(array)
       
 
 def take_a_number(array, new_person)
+  array.push(new_person)
   array.each do |name|
   position = array.index(new_person) + 1
     puts "Welcome, #{new_person}. You are number #{position} in line."
-    array.push(#{new_person})
   end
 end
 
