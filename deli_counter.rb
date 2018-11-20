23a7aeaaf2f0af318bd97ab0e6235f4189c3ba2b
Marie Burns <mcburns2222@gmail.com>
2018-11-20 00:27:58 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -9,7 +9,8 @@ def line(array)
   end
       puts "The line is currently empty."
 end
-...
+
+
 def take_a_number(array, last_person)
   array.each do |index|
     position = index + 1
@@ -26,4 +27,3 @@ def now_serving
   puts "There is nobody waiting to be served!"
   end
 end
-*/
